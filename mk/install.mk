fb1c58bdfbc4acfcdb237813952ffb7d8adaaad3
Stepanov Aleksey <blueingreen@bluig.space>
2026-03-26 19:27:16 +0500

    feat: add DESTDIR variable for install.mk

--- mk/install.mk
+++ mk/install.mk
@@ -1,12 +1,14 @@
+# DESTDIR variable for arch makepkg
+
 .PHONY: install
 
 install: build man
-	install -Dm755 $(TARGET) $(BIN_DIR)
+	install -Dm755 $(TARGET) $(DESTDIR)$(BIN_DIR)
 ifeq ($(MAN_PAGES_ENABLED), 1)
-	install -d $(MAN_DIR)/man1
-	install -d $(MAN_DIR)/man5
-	install -m644 $(DOC_DIR)/$(TARGET).1.gz $(MAN_DIR)/man1/$(TARGET).1.gz
-	install -m644 $(DOC_DIR)/$(TARGET).5.gz $(MAN_DIR)/man5/$(TARGET).5.gz
+	install -d $(DESTDIR)$(MAN_DIR)/man1
+	install -d $(DESTDIR)$(MAN_DIR)/man5
+	install -m644 $(DOC_DIR)/$(TARGET).1.gz $(DESTDIR)$(MAN_DIR)/man1/$(TARGET).1.gz
+	install -m644 $(DOC_DIR)/$(TARGET).5.gz $(DESTDIR)$(MAN_DIR)/man5/$(TARGET).5.gz
 else
 	@echo $(MAN_PAGES_DISABLED_MSG)
 endif
