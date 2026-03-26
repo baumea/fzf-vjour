232f9310f48e1417ebed75df035205efcdb85940
Stepanov Aleksey <blueingreen@bluig.space>
2026-03-26 22:56:07 +0500

    feat: separation of installation

--- mk/install.mk
+++ mk/install.mk
@@ -3,12 +3,15 @@
 .PHONY: install
 
 install: build man
-	install -Dm755 $(TARGET) $(DESTDIR)$(BIN_DIR)
-ifeq ($(MAN_PAGES_ENABLED), 1)
-	install -d $(DESTDIR)$(MAN_DIR)/man1
-	install -d $(DESTDIR)$(MAN_DIR)/man5
-	install -m644 $(DOC_DIR)/$(TARGET).1.gz $(DESTDIR)$(MAN_DIR)/man1/$(TARGET).1.gz
-	install -m644 $(DOC_DIR)/$(TARGET).5.gz $(DESTDIR)$(MAN_DIR)/man5/$(TARGET).5.gz
+ifdef ($(DESTDIR))
+	install -Dm755 $(TARGET) $(DESTDIR)$(BIN_DIR)/$(TARGET)
+	install -Dm644 $(DOC_DIR)/$(TARGET).1.gz $(DESTDIR)$(MAN_DIR)/man1/$(TARGET).1.gz
+	install -Dm644 $(DOC_DIR)/$(TARGET).5.gz $(DESTDIR)$(MAN_DIR)/man5/$(TARGET).5.gz
+else ifeq ($(MAN_PAGES_ENABLED), 1)
+	install -Dm755 $(TARGET) $(BIN_DIR)
+	install -Dm644 $(DOC_DIR)/$(TARGET).1.gz $(MAN_DIR)/man1
+	install -Dm644 $(DOC_DIR)/$(TARGET).5.gz $(MAN_DIR)/man5
 else
 	@echo $(MAN_PAGES_DISABLED_MSG)
+	install -Dm755 $(TARGET) $(BIN_DIR)
 endif
