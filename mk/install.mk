ea9489c2ad3bb382d797472757606b47f0128146
Stepanov Aleksey <blueingreen@bluig.space>
2026-03-27 09:53:12 +0500

    feat: improve man_page_install func in install.mk

--- mk/install.mk
+++ mk/install.mk
@@ -3,17 +3,17 @@
 .PHONY: install
 
 bin_install = install -Dm755 $(TARGET) $(1)/$(TARGET)
-man_page_install = install -Dm644 $(DOC_DIR)/$(TARGET).$(1).gz $(ARCH_PKGDIR)$(MAN_DIR)/man$(1)/$(TARGET).$(1).gz
+man_page_install = install -Dm644 $(DOC_DIR)/$(TARGET).$(1).gz $(2)/man$(1)/$(TARGET).$(1).gz
 
 install: build man
 ifdef ARCH_PKGDIR
 	$(call bin_install,$(ARCH_PKGDIR)$(BIN_DIR))
-	$(call man_page_install,1)
-	$(call man_page_install,5)
+	$(call man_page_install,1,$(ARCH_PKGDIR)$(MAN_DIR))
+	$(call man_page_install,5,$(ARCH_PKGDIR)$(MAN_DIR))
 else ifeq ($(MAN_PAGES_ENABLED), 1)
 	$(call bin_install,$(BIN_DIR))
-	$(call man_page_install,1)
-	$(call man_page_install,5)
+	$(call man_page_install,1,$(MAN_DIR))
+	$(call man_page_install,5,$(MAN_DIR))
 else
 	@echo $(MAN_PAGES_DISABLED_MSG)
 	$(call bin_install,$(BIN_DIR))
